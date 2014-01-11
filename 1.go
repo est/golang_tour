6f90cb10641857a4ea7420b9d303e00c7a363de1
est <electronicstar@126.com>
2014-01-11 17:58:35 +0800

    course 36

--- 1.go
+++ 1.go
@@ -45,6 +45,19 @@ func Sqrt(x float64) float64 {
     return z
 }
 
+
+
+type Vertex struct {
+    X, Y int
+}
+
+var (
+    p = Vertex{1, 2}  // has type Vertex
+    q = &Vertex{1, 2} // has type *Vertex
+    r = Vertex{X: 1}  // Y:0 is implicit
+    s = Vertex{}      // X:0 and Y:0
+)
+
 func main() {
     fmt.Println("My favorite number is", rand.Intn(10))
     fmt.Printf("Now you have %g problems.",
@@ -90,6 +103,65 @@ func main() {
     // if and for exercise
     // http://tour.golang.org/#25
     fmt.Println(Sqrt(2), math.Sqrt(2))
+
+    // structs nigga
+    fmt.Println(p, q, q.X, r, s)
+
+    // The expression new(T) allocates a zeroed T value and returns a pointer to it.
+    var t1 *Vertex = new(Vertex)
+    // alternative syntax
+    t2 := new(Vertex)
+    fmt.Println(t1, t2)
+
+    // arrays
+    var a [2]string
+    a[0] = "Hello"
+    a[1] = "World"
+    fmt.Println(a[0], a[1])
+    fmt.Println(a)
+
+    // array slice
+    p := []int{2, 3, 5, 7, 11, 13}
+    fmt.Println("p ==", p)
+
+    for i := 0; i < len(p); i++ {
+        fmt.Printf("p[%d] == %d\n", i, p[i])
+    }
+    // []T is a slice with elements of type T.
+
+
+    p2 := []int{2, 3, 5, 7, 11, 13}
+    fmt.Println("p ==", p2)
+    fmt.Println("p[1:4] ==", p2[1:4])
+    // missing low index implies 0
+    fmt.Println("p[:3] ==", p2[:3])
+    // missing high index implies len(s)
+    fmt.Println("p[4:] ==", p2[4:])
+
+    // the `make` function
+    a2 := make([]int, 5)
+    fmt.Println("a", a2)
+    b2 := make([]int, 0, 5)
+    fmt.Println("b", b2)
+    c2 := b2[:2]
+    fmt.Println("c", c2)
+    d2 := c2[2:5]
+    fmt.Println("d", d2)
+    // @ToDo: what is cap() ?
+
+    // nil slice
+    var z3 []int
+    fmt.Println(z3, len(z3), cap(z3))
+    if z3 == nil {
+        fmt.Println("nil!")
+    }
+
+    // internal :
+    // http://golang.org/doc/articles/slices_usage_and_internals.html
+
+    // test a rest
+    // http://tour.golang.org/#36
+
 
 
 }
